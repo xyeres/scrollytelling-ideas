d2e2c0eb072416b8c3c8328f1778451823b1b47a
Michael Carr <xyeres@me.com>
2022-08-14 14:15:14 -0700

    fade images on scroll

--- project-with-images/app.js
+++ project-with-images/app.js
@@ -1,1 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".img").forEach((img) => {
+  gsap.fromTo(
+    img,
+    {
+      opacity: 0.1,
+      filter: "grayscale(100%)",
+    },
+    {
+      duration: 1.5,
+      opacity: 1,
+      filter: "grayscale(0%)",
+      scrollTrigger: {
+        trigger: img,
+        start: "15% 95%",
+        end: "bottom 60%",
+        markers: true,
+        toggleActions: "restart reverse none reverse",
+      },
+    }
+  );
+});
